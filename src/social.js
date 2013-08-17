dd88846bb76340abe509fc7cce03fd2ac0226b1f
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-08-17 13:21:03 -0300

    Improvements to the code :metal:

--- src/social.js
+++ src/social.js
@@ -1,6 +1,6 @@
 /*
 *
-* Social 2.0
+* Social 2.1
 * Copyright 2013, Pedro Rogerio (https://github.com/pinceladasdaweb)
 * Description: Lazy Loader Social Buttons
 * Licensed under the WTFPL licenses (http://www.wtfpl.net/).
@@ -8,18 +8,17 @@
 */
 
 var Social = {
-    init: function() {
+    init: function () {
         this.fetch();
     },
-    getScript: function(url, callback) {
-        var head = document.getElementsByTagName("head")[0], done = false;
-        var script = document.createElement("script");
+    getScript: function (url, callback) {
+        var head   = document.getElementsByTagName("head")[0], done = false, script = document.createElement("script");
         script.src = url;
 
-        script.onload = script.onreadystatechange = function(){
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        script.onload = script.onreadystatechange = function () {
+            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
                 done = true;
-                if(callback) {
+                if (callback && typeof ("callback") === "function") {
                     callback();
                 }
             }
@@ -27,34 +26,33 @@ var Social = {
 
         head.appendChild(script);
     },
-    fetch: function() {
+    fetch: function () {
         var self = this;
 
-        if(typeof (twttr) != 'undefined') {
+        if (typeof (twttr) !== 'undefined') {
             twttr.widgets.load();
         } else {
             self.getScript('//platform.twitter.com/widgets.js');
         }
 
-        if (typeof (FB) != 'undefined') {
-            FB.init({ status: true, cookie: true, xfbml: true });
+        if (typeof (FB) !== 'undefined') {
+            FB.init({ status: true, cookie: true, xfbml: true, oauth: true });
         } else {
             self.getScript("//connect.facebook.net/pt_BR/all.js#xfbml=1", function () {
-                FB.init({status:true, cookie:true, xfbml:true, oauth:true});
+                FB.init({ status: true, cookie: true, xfbml: true, oauth: true });
             });
         }
 
-        if (typeof (gapi) != 'undefined') {
-            var matches = document.body.querySelectorAll('.g-plusone');
-            var index = 0, length = matches.length;
-            while (index++ < length) {
+        if (typeof (gapi) !== 'undefined') {
+            var matches = document.body.querySelectorAll('.g-plusone'), index = 0, length = matches.length;
+            while (index += 1 < length) {
                 gapi.plusone.go();
             }
         } else {
             self.getScript('//apis.google.com/js/plusone.js');
         }
 
-        if (typeof (IN) != 'undefined') {
+        if (typeof (IN) !== 'undefined') {
             IN.parse();
         } else {
             self.getScript("//platform.linkedin.com/in.js");
